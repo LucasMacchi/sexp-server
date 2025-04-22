a48402fc4f33db52e2a67e605128e981abf20bc4
Lucas Macchi <lucasmacchi25@gmail.com>
2025-04-22 17:10:25 -0300

    create exp fixed

--- src/expediente/expediente.service.ts
+++ src/expediente/expediente.service.ts
@@ -27,15 +27,14 @@ export class ExpedienteService {
         return exps
     }
     async createExpediente (exp: expedienteDto){
-        const adddes = `, '${exp.descripcion}'`
         const sql = `INSERT INTO public.glpi_sexp_expediente
-        (service_id,user_id, numero_exp, concepto, periodo, 
+        (service_id, user_id, numero_exp, concepto, periodo, 
         fecha_presentacion, fecha_ult_mod, nro_factura, 
-        empresa_id, estado_id, importe${exp.descripcion && ', descripcion'})
+        empresa_id, estado_id, importe, descripcion)
         VALUES(${exp.servicio_id},${exp.user_id} , '${exp.numero_exp}', 
         '${exp.concepto}', '${exp.periodo}', '${exp.fecha_presentacion}', 
         '${exp.fecha_presentacion}', '${exp.nro_factura}', ${exp.empresa_id},
-         ${exp.estado_id}, ${exp.importe}${adddes});`
+         ${exp.estado_id}, ${exp.importe}, '${exp.descripcion}');`
         
         const conn = clientReturner()
         await conn.connect()
