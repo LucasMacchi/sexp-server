8bb4ec3558ceabd99a82fb153a353fbb354867ce
LucasMacchi <lucasmacchi25@gmail.com>
2025-07-08 15:36:54 -0300

    new ersion

--- src/expediente/expediente.service.ts
+++ src/expediente/expediente.service.ts
@@ -10,28 +10,8 @@ export class ExpedienteService {
         const conn = clientReturner()
         await conn.connect()
         console.log(data)
-        const editCon = `UPDATE public.glpi_sexp_expediente SET concepto='${data.concepto}' WHERE exp_id = ${id};`
-        const editInv = `UPDATE public.glpi_sexp_expediente SET invitacion='${data.invitacion}' WHERE exp_id = ${id};`
-        const editOrdenC = `UPDATE public.glpi_sexp_expediente SET orden_compra='${data.orden_compra}' WHERE exp_id = ${id};`
-        const editImporte = `UPDATE public.glpi_sexp_expediente SET importe=${data.importe} WHERE exp_id = ${id};`
-        const editNroExp = `UPDATE public.glpi_sexp_expediente SET numero_exp='${data.numero_exp}' WHERE exp_id = ${id};`
-        const editNroF = `UPDATE public.glpi_sexp_expediente SET nro_factura='${data.nro_factura}' WHERE exp_id = ${id};`
-        const editEstado = `UPDATE public.glpi_sexp_expediente SET estado_id=${data.estado_id} WHERE exp_id = ${id};`
-        const editFecha = `UPDATE public.glpi_sexp_expediente SET fecha_ult_mod='${data.ultima_mod}' WHERE exp_id = ${id};`
-        const editDes = `UPDATE public.glpi_sexp_expediente SET descripcion='${data.descripcion}' WHERE exp_id = ${id};`
-        const editFechaFacturacion = `UPDATE public.glpi_sexp_expediente SET fecha_facturacion='${data.fecha_facturacion}' WHERE exp_id = ${id};`
-        const editFechaTesoreria = `UPDATE public.glpi_sexp_expediente SET fecha_tesoreria='${data.fecha_tesoreria}' WHERE exp_id = ${id};`
-        await conn.query(editInv)
-        await conn.query(editOrdenC)
-        if(data.concepto) await conn.query(editCon)
-        if(data.fecha_tesoreria) await conn.query(editFechaTesoreria)
-        if(data.fecha_facturacion) await conn.query(editFechaFacturacion)
-        if(data.importe) await conn.query(editImporte)
-        if(data.nro_factura) await conn.query(editNroF)
-        if(data.numero_exp) await conn.query(editNroExp)
-        if(data.estado_id) await conn.query(editEstado)
-        if(data.ultima_mod) await conn.query(editFecha)
-        if(data.descripcion) await conn.query(editDes)
+        const sql = `UPDATE public.glpi_sexp_expediente SET ${data.prop}='${data.value}' WHERE exp_id = ${id};`
+        await conn.query(sql)
         await conn.end()
         return `Expediente actualizado.`
     }
@@ -58,5 +38,24 @@ export class ExpedienteService {
         await conn.connect()
         await conn.query(sql)
         await conn.end()
+    }
+    async getUniqExpediente (id: number) {
+        const sql = `SELECT * FROM public.glpi_sexp_expediente WHERE exp_id = ${id};`
+        const conn = clientReturner()
+        await conn.connect()
+        const exps = (await conn.query(sql)).rows[0]
+        //console.log(exps)
+        await conn.end()
+        return exps
+    }
+
+    async getByNro (nro: string) {
+        const sql = `SELECT exp_id FROM public.glpi_sexp_expediente WHERE numero_exp = '${nro}';`
+        const conn = clientReturner()
+        await conn.connect()
+        const exps = (await conn.query(sql)).rows[0]
+        console.log(exps)
+        await conn.end()
+        return exps
     }
 }
