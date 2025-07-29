a37b76822ebec0ede55219b0314e54c58432a9cc
LucasMacchi <lucasmacchi25@gmail.com>
2025-07-29 17:05:24 -0300

    txt arrays

---
+++ src/Dtos/addTicketDto.ts
@@ -0,0 +1,69 @@
+import { IsNotEmpty, IsString, IsNumber } from "class-validator";
+
+
+export default class {
+
+    @IsString()
+    @IsNotEmpty()
+    fecha: string
+
+    @IsString()
+    @IsNotEmpty()
+    comprobante: string
+
+    @IsString()
+    @IsNotEmpty()
+    tipo: string
+
+    @IsString()
+    @IsNotEmpty()
+    pv: string
+
+    @IsString()
+    @IsNotEmpty()
+    nro: string
+
+    @IsString()
+    @IsNotEmpty()
+    prov_cuit: string
+
+    @IsString()
+    @IsNotEmpty()
+    prov_name: string
+
+    @IsNumber()
+    @IsNotEmpty()
+    prov_cod: number
+
+    @IsNumber()
+    @IsNotEmpty()
+    provsiv_cod: number
+
+    @IsNumber()
+    @IsNotEmpty()
+    proprv_codigo: number
+
+    @IsNumber()
+    @IsNotEmpty()
+    neto: number
+
+    @IsNumber()
+    @IsNotEmpty()
+    ivapor: number
+
+    @IsNumber()
+    @IsNotEmpty()
+    iva: number
+
+    @IsNumber()
+    @IsNotEmpty()
+    total: number
+
+    @IsString()
+    @IsNotEmpty()
+    concepto: string
+
+    @IsString()
+    @IsNotEmpty()
+    concepto_cod: string
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,13 @@ import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
 import { ExpedienteModule } from './expediente/expediente.module';
 import { DataModule } from './data/data.module';
+import { TicketsController } from './tickets/tickets.controller';
+import { TicketsModule } from './tickets/tickets.module';
+import { TicketsService } from './tickets/tickets.service';
 
 @Module({
-  imports: [UserModule, ExpedienteModule, DataModule],
-  controllers: [AppController, UserController, ExpedienteController],
-  providers: [AppService, ExpedienteService, UserService],
+  imports: [UserModule, ExpedienteModule, DataModule, TicketsModule],
+  controllers: [AppController, UserController, ExpedienteController, TicketsController],
+  providers: [AppService, ExpedienteService, UserService, TicketsService],
 })
 export class AppModule {}
